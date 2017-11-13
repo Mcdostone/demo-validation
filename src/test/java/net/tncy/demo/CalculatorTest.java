8ed2a2720b946ee9db603a261eab6ee717624641
Yann Prono <yann.prono@telecomnancy.net>
2017-11-13 16:39:50 +0100

    Add calculator

---
+++ src/test/java/net/tncy/demo/CalculatorTest.java
@@ -0,0 +1,37 @@
+package net.tncy.demo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+
+    private Calculator c;
+    @Before
+    public void setup() {
+        this.c = new Calculator();
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals("Should be 3",3, this.c.add(1,2), 0);
+    }
+
+    @Test
+    public void testSub() {
+        assertEquals("Should be 3",3, this.c.substract(5,2), 0);
+    }
+
+    @Test
+    public void testMul() {
+        assertEquals("Should be 3",3, this.c.multiply(1,3), 0);
+    }
+
+    @Test
+    public void testDiv() {
+        assertEquals("Should be 3",3, this.c.multiply(9,3), 0);
+    }
+    
+}
